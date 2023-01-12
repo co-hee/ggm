a37c9372dd99424b80bc109dba5fe1acc784419d
Seonghee Yun <104363065+intacthee@users.noreply.github.com>
2023-01-12 12:43:53 +0900

    Add files via upload

---
+++ doc/assets.php
@@ -0,0 +1,11 @@
+<? 
+$as_company = '깔끄미청소';
+$as_tell = '051-783-2317';
+$as_mail = 'sipossa@naver.com';
+$as_address = '경상북도 울진군 후포면 후포리 145';
+$as_company_number = '064-09-424';
+$as_name = '이창훈';
+$as_slogan = '부산 아파트 입주청소 이사청소';
+$as_description = '부산 아파트입주청소 부산 양산 김해 기장 아파트입주청소 이사청소 오피스텔청소 상가청소 전문업체 금맥';
+$as_color = ['#da544b', '#f4cb4f', '#337689', '#6fa476'];
+?>
